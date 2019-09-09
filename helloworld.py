a8f08ad856dfeb129f31d02c3068cbe12a212d16
oaansari <uniquename@umich.edu>
2019-09-09 13:36:59 -0400

    Pushing some python code

---
+++ helloworld.py
@@ -0,0 +1,12 @@
+from random import randint
+
+print ("Osamah Ansari")
+
+num1 = randint(1,100)
+num2 = randint(1,100)
+total = num1 + num2 
+
+print(num1)
+print(num2)
+print("Sum = " + str(total))
+print("Average = " + str(total/2.0))
