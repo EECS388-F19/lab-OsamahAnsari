34942711f5be0e22c8da84394bcb231d9474cd79
oaansari <uniquename@umich.edu>
2019-09-09 13:46:22 -0400

    Sorts and prints longest name

---
+++ names.py
@@ -0,0 +1,12 @@
+students = ["Perry", "Ian", "Michael"]
+students.sort();
+
+first_name = students[0]
+first_name = first_name[:-1]
+print(first_name)
+
+longest_name = ''
+for student in students:
+	if len(student) > len(longest_name):
+		longest_name = student
+print(longest_name)
